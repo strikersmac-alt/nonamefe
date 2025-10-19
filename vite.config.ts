9a40305290262a3e56dbecd81230ee420b06c7e1
dhananjaysingh10 <itsdhananjaysingh04@gmail.com>
2025-10-19 11:47:46 +0530

    use the network ip to access fe on mobile during dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
+    host: true,            
+    port: 5173, 
     allowedHosts: [
       '08a00fe8454c.ngrok-free.app' 
     ]

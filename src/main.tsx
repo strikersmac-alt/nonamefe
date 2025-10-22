c55aee78525001899959a9d744c0e73b7c6f241a
dhananjaysingh10 <itsdhananjaysingh04@gmail.com>
2025-10-22 19:32:42 +0530

    dre

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -31,7 +31,7 @@ const RootWrapper: React.FC = () => {
 
 createRoot(document.getElementById('root')!).render(
   
-  // <StrictMode>
+  <StrictMode>
     <RootWrapper />
-  // </StrictMode>
+  </StrictMode>
 )
